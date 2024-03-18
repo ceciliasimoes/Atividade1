f0156ec62fe79d99a1103d876b38b0f33fbee4d9
Ana Cecília Simões Chalegre <130169684+ceciliasimoes@users.noreply.github.com>
2024-03-18 20:51:11 +0000

    Alterando imagens para cdn

--- dados/src/components/Dado.js
+++ dados/src/components/Dado.js
@@ -1,4 +1,13 @@
 import React, { useState } from 'react';
+const dados = [
+  "https://cdn.glitch.global/d06ef131-6a92-4c03-9b83-0c7f07ea77eb/dado1.png?v=1709855087643",
+  "https://cdn.glitch.global/d06ef131-6a92-4c03-9b83-0c7f07ea77eb/dado2.png?v=1709855088164",
+  "https://cdn.glitch.global/d06ef131-6a92-4c03-9b83-0c7f07ea77eb/dado3.png?v=1709855088659",
+  "https://cdn.glitch.global/d06ef131-6a92-4c03-9b83-0c7f07ea77eb/dado4.png?v=1709855089180",
+  "https://cdn.glitch.global/d06ef131-6a92-4c03-9b83-0c7f07ea77eb/dado5.png?v=1709855089711",
+  "https://cdn.glitch.global/d06ef131-6a92-4c03-9b83-0c7f07ea77eb/dado6.png?v=1709855090308",
+];
+
 
 export function Dado({condicao, toggleAtivo}) {
   const [dado, setDado] = useState(Math.floor(Math.random() * 6) + 1);
@@ -10,11 +19,11 @@ export function Dado({condicao, toggleAtivo}) {
   }
   
  
-
+  
   return (
     <>
     <div style={{justifyContent : 'center'}}>
-      <img src={`../images/dado${dado}.png`} alt={`Dado ${dado}`} />
+      <img src={dados[dado]} alt={`Dado ${dado}`} />
       <h2>Seu número foi : {dado}</h2>
       <button onClick={jogarDado} disabled={condicao} style={{borderRadius :'10px', border: 'none', padding: '20px', fontSize:'20px', color:'Purple'}}>Jogar dado</button>
       </div> 
