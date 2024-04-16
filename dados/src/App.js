2837995caa258b58c289c51d52cb6aaa3eda90bb
Ana Cecília Simões Chalegre <130169684+ceciliasimoes@users.noreply.github.com>
2024-04-15 23:51:02 -0300

    Update App.js

--- dados/src/App.js
+++ dados/src/App.js
@@ -5,36 +5,57 @@ import {Placar} from './components/Placar';
 
 
 function App() {
-  const [desativo, setDesativo] = useState(true);
-
-  const toggleDesativo = () => {
-    setDesativo(!desativo);
-    setCount(count+1);
-  };
-
   const [count, setCount] = useState(0);
   const [number1, setNumber1] = useState(0);
   const [number2, setNumber2] = useState(0);
   const [player1, setPlayer1] = useState(0);
   const [player2, setPlayer2] = useState(0);
+  
+
+  const [desativo, setDesativo] = useState(true);
+
+  const toggleDesativo = () => {
+    setDesativo(!desativo);
+    setCount(prevCount => prevCount + 1);
+  };
 
 
-useEffect(() =>{
-  if( count % 2 === 0){
-    if(number1 >number2){
-      setPlayer1(player1+1);
-    } else if(number2>number1){
-      setPlayer2(player2+1);
+
+  useEffect(() => {
+    if (count % 2 === 0) {
+      if (number1 > number2) {
+        setPlayer1(player1 + 1);
+      } else if (number2 > number1) {
+        setPlayer2(player2 + 1);
+      }
     }
-  }
-},[count]);
+  }, [count, number1, number2]);
 
+
+useEffect(() => {
+  if ((count / 2) === 5) {
+    if (player1 > player2) {
+      alert("O jogador 1 venceu!");
+    } else if (player2 > player1) {
+      alert("O jogador 2 venceu!");
+    }
+    }
+  }, [player1, player2, count]);
+
+  const handleRestart = () => {
+    setPlayer1(0);
+    setPlayer2(0);
+    setCount(0);
+  };
+
+ let partida = Math.round(count/2);
 
 
   return (
     <div className="App" style={{alignItems:'revert'}}>
       <header className="App-header">
         <h1>Jogue seu dado!</h1>
+        <h2>Melhor de cinco! Essa é a partida <strong>{partida}ª</strong>!</h2>
         
         <div style={{display : 'flex', justifyContent : 'space-between', gap : '50px'}}>
         <Dado condicao={desativo} toggleDesativo={toggleDesativo} setNumber={setNumber1} />
@@ -42,6 +63,10 @@ useEffect(() =>{
         <Dado condicao={!desativo} toggleDesativo={toggleDesativo} setNumber ={setNumber2} />
         </div>
         <Placar  player1={player1} player2={player2}/>
+        {partida === 5 && (
+          <button onClick={handleRestart} style={{borderRadius :'10px', border: 'none', padding: '30px', fontSize:'30px', color:'Purple'}}>Reiniciar partida</button>
+        )}
+
       </header>
     </div>
   );
